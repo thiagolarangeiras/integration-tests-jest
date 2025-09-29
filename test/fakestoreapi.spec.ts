20f2c42c045b712be191c4fb980f4defad50a556
thiagolarangeiras <thiago.larangeira50@gmail.com>
2025-09-29 11:12:18 -0300

    testando

--- test/fakestoreapi.spec.ts
+++ test/fakestoreapi.spec.ts
@@ -16,6 +16,7 @@ describe('Loja Falsa', () => {
       await p
         .spec()
         .get(`${baseUrl}/products`)
+        .withHeaders('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36')
         .inspect()
         .expectStatus(StatusCodes.OK);
     });
